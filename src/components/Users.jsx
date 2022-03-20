3e3fb05b5868344d6b5e2d9ad910688d1a344bc8
Nitish Kumar <81190422+Nitish8800@users.noreply.github.com>
2022-03-21 02:00:51 +0530

    done

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -2,19 +2,19 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export const Users = () => {
-  const [list, setList] = useState();
+  const [list, setList] = useState([]);
 
   useEffect(() => {
     axios.get("https://reqres.in/api/users").then(({ data }) => {
       // console.log(data);
-      setList([...data.data]);
+      setList([...data]);
     });
   }, []);
 
   return (
     <div>
       {list.map((e) => (
-        <div>
+        <div key={e.id}>
           {e.id}. {e.first_name}
         </div>
       ))}
