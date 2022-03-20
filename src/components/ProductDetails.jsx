9b86eba50e9cf1d35669d34f2ddc396149023866
Nitish Kumar <81190422+Nitish8800@users.noreply.github.com>
2022-03-21 01:37:08 +0530

    c

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,3 +1,11 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 export const ProductDetails = () => {
-  return <div>You are seeing details of product : {"Laptop"}</div>;
+  const { id } = useParams();
+  useEffect(() => {
+    // use Id
+  }, []);
+
+  return <div>You are seeing details of product : {id}</div>;
 };
