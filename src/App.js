a018d2b08d8663ecd9548515f75df6d07127e435
Nitish Kumar <81190422+Nitish8800@users.noreply.github.com>
2022-03-21 00:41:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import "./styles.css";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={"Hello"} />
+        <Route path="/about" element={"About"} />
+      </Routes>
+    </div>
+  );
 }
