9b7d1c47b3eb7f4fb9d6ae503d2a43ffe7b313e1
Nitish Kumar <81190422+Nitish8800@users.noreply.github.com>
2022-03-21 02:37:11 +0530

    pending

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 export const Login = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <input type="text" placeholder="Enter Username" />
       <input type="text" placeholder="Enter Password" />
-      <input type="submit" value="submit" />
+      <button
+        onClick={() => {
+          //
+          navigate("/");
+        }}
+      >
+        Login
+      </button>
     </div>
   );
 };
